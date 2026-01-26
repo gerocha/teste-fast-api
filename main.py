403147586bfb43bb8e1f2d32f86db059f78f928e
Gelso Junior <gelso.rocha@pinpag.com.br>
2026-01-25 21:56:37 -0300

    feat: hello world

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from teste-fast-api!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
