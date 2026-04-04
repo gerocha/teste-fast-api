2f098e6bc3d4edc35ba3bba32bd4be18347f4249
Gelso Junior <gelso.rocha@pinpag.com.br>
2026-04-04 00:23:34 -0300

    feat: add db_url env var

--- stock/infra/db/__init__.py
+++ stock/infra/db/__init__.py
@@ -1,9 +1,15 @@
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker
 
-connect_args = {"check_same_thread": False}
+from stock.settings import DB_URL
+
+connect_args = {}
+
+if 'sqlite' in DB_URL:
+    connect_args = {"check_same_thread": False}
+
 engine = create_engine(
-    "sqlite+pysqlite:///:memory:", connect_args=connect_args
+    DB_URL, connect_args=connect_args
 )
 
 metadata = MetaData()
